dee88d973fc69eb0950704d502f34f1e2c471152
manley1971 <manley@stanfordalumni.org>
2016-04-06 16:25:33 -0700

    Revert "Revert "json test""
    This reverts commit 4216ff1e772939987268130af867a9533576887a.

--- server/startup.js
+++ server/startup.js
@@ -5,7 +5,7 @@
 
 if (Meteor.isServer){
 	Meteor.startup(function(){
-		Songs.delete({});
+		Songs.remove({});
 		if (!Songs.findOne()){
 		console.log("no songs yet... creating from filesystem");
 		// pull in the NPM package 'fs' which provides
